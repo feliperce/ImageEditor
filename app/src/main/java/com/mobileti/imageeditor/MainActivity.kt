527e5d53da173360608e6b887b2cd0bc2f2b48b6
Felipe Rodrigues <felipe.celestino@ftd.com.br>
2018-12-18 17:27:04 -0200

    implemented main acitivty

--- app/src/main/java/com/mobileti/imageeditor/MainActivity.kt
+++ app/src/main/java/com/mobileti/imageeditor/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.mobileti.imageeditor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.mobileti.imageeditor.EditorActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editImageFab.setOnClickListener {
+            val intent = Intent(this, EditorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
