282504e2658836523877f30eca0d92ca171bb7fb
Felipe Rodrigues <felipe.rce@gmail.com>
2021-11-22 00:44:55 -0300

    buildSrc creation

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,8 @@
+object AppConfig {
+    const val compileSdk = 31
+    const val minSdk = 15
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+    const val buildToolsVersion = "29.0.3"
+}
