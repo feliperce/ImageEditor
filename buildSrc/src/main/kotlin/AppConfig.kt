870f0cacb52ef3089f9f4a2e7d5d908a1b532c12
Felipe Rodrigues <felipe.rce@gmail.com>
2021-11-22 01:40:15 -0300

    Merge pull request #5
    * quotes replacement
    
    * function replacement
    
    * buildSrc creation
    
    * gradle to kts, Dependencies refactor

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,8 @@
+object AppConfig {
+    const val compileSdk = 31
+    const val minSdk = 15
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "0.5.2"
+    const val buildToolsVersion = "29.0.3"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+    const val gradle = "7.0.3"
+    const val kotlin = "1.5.31"
+    const val appCompat = "1.3.1"
+    const val constraintLayout = "2.1.2"
+    const val jUnit = "4.13.2"
+    const val testRunner = "1.4.0"
+    const val espresso = "3.2.0"
+    const val material = "1.0.0"
+    const val recyclerView = "1.2.1"
+    const val coroutines = "1.5.2"
+    const val lifecycle = "2.4.0"
+}
